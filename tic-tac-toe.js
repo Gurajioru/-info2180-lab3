1b33bbe1933d884d4e524aee34a60ad77a2954db
Jelani Smith <rathalosrocks@gmail.com>
2021-10-08 16:13:31 -0500

    Added hover highlight

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -2,6 +2,27 @@ window.onload = function(){
     var tileNodes= document.getElementById('board').childNodes;
     tileNodes.forEach(element => {
         element.className="square";
+        element.addEventListener('mouseover',function(event){
+            event.preventDefault();
+            element.classList.add('hover')
+            console.log("hovering");
+        })
+        element.addEventListener('mouseout',function(event){
+            event.preventDefault();
+            element.classList.remove('hover');
+        })
         console.log("add");
     });
 }
+
+
+
+//var hoverArray=document.getElementById('board').childNodes;
+//console.log(hoverArray);
+//hoverArray.forEach(element=> {
+//    element.addEventListener('mouseover',function(event){
+ //       element.className += "hover";
+   //     console.log("hovering");
+    //})
+//})
+
